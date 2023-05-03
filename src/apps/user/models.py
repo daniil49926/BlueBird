2221bd4126a57c5119d3db0d42a29a216efab9cd
kaMata0 <daniil49926@gmail.com>
2023-05-03 22:32:29 +0300

    added models for the app user

---
+++ src/apps/user/models.py
@@ -0,0 +1,19 @@
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+from core.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "User"
+    id: int = Column(Integer, primary_key=True)
+    name: str = Column(String, nullable=False)
+    api_key: str = Column(String, nullable=False)
+
+
+class FollowersReferences(Base):
+    __tablename__ = "FollowersReferences"
+    id: int = Column(Integer, primary_key=True)
+    user_id: int = Column(ForeignKey("User.id", ondelete="CASCADE", onupdate="CASCADE"), index=True, nullable=False)
+    follow: int = Column(ForeignKey("User.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
