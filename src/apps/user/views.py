4361e0f53f8aa79decbc8ef323d235821fb04a9b
kaMata0 <daniil49926@gmail.com>
2023-05-05 19:18:13 +0300

    refactoring user apps

--- src/apps/user/views.py
+++ src/apps/user/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 
 from apps.user.utils import (
@@ -14,40 +14,39 @@ v1 = APIRouter()
 @v1.get(path="/me")
 async def get_me(api_key: str, session=Depends(get_db)) -> JSONResponse:
     user = await get_user_by_key(session=session, api_key=api_key)
-    if user:
-        (
-            response_following,
-            response_follower,
-        ) = await get_follower_and_following_by_user(session=session, user_in=user)
-        return JSONResponse(
-            status_code=200,
-            content={
-                "result": "true",
-                "user": {
-                    "id": user.id,
-                    "name": user.name,
-                    "followers": response_follower,
-                    "following": response_following,
-                },
-            },
-        )
-    else:
-        return JSONResponse(
-            status_code=404,
-            content={
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
                 "result": "false",
                 "error_type": None,
-                "error_message": "User not found",
+                "error_message": "Not authenticated",
             },
         )
+    (
+        response_following,
+        response_follower,
+    ) = await get_follower_and_following_by_user(session=session, user_in=user)
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "result": "true",
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "followers": response_follower,
+                "following": response_following,
+            },
+        },
+    )
 
 
 @v1.get("/{uid}")
 async def get_user(uid: int, api_key: str, session=Depends(get_db)) -> JSONResponse:
     if not await get_user_by_key(session=session, api_key=api_key):
-        return JSONResponse(
-            status_code=404,
-            content={
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
                 "result": "false",
                 "error_type": None,
                 "error_message": "Not authenticated",
@@ -55,9 +54,9 @@ async def get_user(uid: int, api_key: str, session=Depends(get_db)) -> JSONRespo
         )
     user = await get_user_by_id(session=session, id_=uid)
     if not user:
-        return JSONResponse(
-            status_code=404,
-            content={
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
                 "result": "false",
                 "error_type": None,
                 "error_message": "User not found",
@@ -67,7 +66,7 @@ async def get_user(uid: int, api_key: str, session=Depends(get_db)) -> JSONRespo
         session=session, user_in=user
     )
     return JSONResponse(
-        status_code=200,
+        status_code=status.HTTP_200_OK,
         content={
             "result": "true",
             "user": {
