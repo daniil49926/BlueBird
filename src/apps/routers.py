f2cd6b60d5a955fa1da2ed9d92afd50bc2c74b0a
kaMata0 <daniil49926@gmail.com>
2023-05-13 21:08:12 +0300

    added router for tweets

--- src/apps/routers.py
+++ src/apps/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from apps.tweet.views import v1 as tweet_v1
 from apps.user.views import v1 as user_v1
 
 router = APIRouter()
 
 router.include_router(router=user_v1, prefix="/api/users", tags=["users"])
+router.include_router(router=tweet_v1, prefix="/api/tweets", tags=["tweets"])
