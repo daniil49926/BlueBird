53ec45ebb57a38b1dc1331b4fde643070b61ed87
kaMata0 <daniil49926@gmail.com>
2023-05-18 21:09:17 +0300

    added tweets models

---
+++ src/apps/tweet/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from core.db.database import Base
+
+
+class Tweet(Base):
+    __tablename__ = "Tweet"
+    id: int = Column(Integer, primary_key=True)
+    content: str = Column(String, nullable=False)
+    author: int = Column(
+        ForeignKey("User.id", ondelete="CASCADE", onupdate="CASCADE"),
+        index=True,
+        nullable=False,
+    )
+
+
+class TweetMediaReferences(Base):
+    __tablename__ = "TweetMediaReferences"
+    tweet_id: int = Column(
+        ForeignKey("Tweet.id", ondelete="CASCADE", onupdate="CASCADE"),
+        index=True,
+        nullable=False,
+    )
+    media_id: int = Column(
+        ForeignKey("Media.media_id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False
+    )
+
+
+class TweetLikes(Base):
+    __tablename__ = "TweetLikes"
+    tweet_id: int = Column(
+        ForeignKey("Tweet.id", ondelete="CASCADE", onupdate="CASCADE"),
+        index=True,
+        nullable=False,
+    )
+    user_id: int = Column(
+        ForeignKey("User.id", ondelete="CASCADE", onupdate="CASCADE"),
+        index=True,
+        nullable=False,
+    )
